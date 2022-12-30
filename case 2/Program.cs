71ca9566f375d82f2e99046a3fb57d68df7c349f
Marina_Arnaut <skital87@mail.ru>
2022-12-30 14:02:28 +1000

    56 case

--- case 2/Program.cs
+++ case 2/Program.cs
@@ -14,29 +14,21 @@
 // Программа считает сумму элементов в каждой строке и выдаёт 
 // номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] GetArray(int a, int b, int minValue, int maxValue)
+int[,] CreateArray(int a, int b)
 {
-    int[,] result = new int[a, b];
-    for (int i = 0; i < a; i++)
+    Console.WriteLine("Заданный массив: ");
+    int[,] matrix = new int[a, b];
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
     {
-        for (int j = 0; j < b; j++)
+        for (int colums = 0; colums < matrix.GetLength(1); colums++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue);
+            matrix[rows, colums] = new Random().Next(1, 10);
+            Console.Write($"{matrix[rows, colums]} ");
         }
-    }
-    return result;
-}
-
-void PrintArray(int[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write(inArray[i, j] + " ");
-        }
+        Console.WriteLine();
     }
     Console.WriteLine();
+    return matrix;
 }
 
 void MinSumRows(int[,] inArray)
@@ -58,11 +50,5 @@ void MinSumRows(int[,] inArray)
     Console.Write($"Номер строки с наименьшей суммой элементов: {indexMinRow}");
 }
 
-Console.Write("Введите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов массива: ");
-int colums = int.Parse(Console.ReadLine()!);
-int[,] array = GetArray(rows, colums, 0, 10);
-PrintArray(array);
 Console.WriteLine();
-MinSumRows(array);
+MinSumRows(CreateArray(4, 4));
